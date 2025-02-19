45338055663ade8fd20369fed1fd8acbc1ac514e
yoshiki44 <ys44oct@gmail.com>
2025-02-19 22:59:20 +0900

    一覧ページの作成（５ページ分）

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,22 +2,39 @@ class HomeController < ApplicationController
   require 'net/http'
   require 'json'
 
-  # 安全にAPIリクエストを送るメソッド
-  def fetch_json(url)
-    uri = URI.parse(url)
-    response = Net::HTTP.get(uri)
-    JSON.parse(response) rescue {}  # JSONのパースに失敗したら空のハッシュを返す
-  end
-
-  def top
+  def index
     api_key = ENV['TMDB_API']
+    base_url = "https://api.themoviedb.org/3"
+    total_pages_to_fetch = 3
+
+    @current_page = (params[:page] || 1).to_i
+
     if params[:looking_for].present?
-      movie_title = params[:looking_for]
-      url = "https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&language=ja&query=" + URI.encode_www_form_component(movie_title)
+      search_url = "#{base_url}/search/movie?api_key=#{api_key}&language=ja&query=#{URI.encode_www_form_component(params[:looking_for])}"
     else
-      url = "https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=ja"
+      search_url = "#{base_url}/movie/popular?api_key=#{api_key}&language=ja"
     end
 
-    @movies = fetch_json(url)["results"] || []  # 直接 `results` 配列を取得
+    @movies = []
+
+    (1..total_pages_to_fetch).each do |page|
+      url = "#{search_url}&page=#{page}"
+      puts "Fetching URL: #{url}"  # 確認用ログ
+
+      response = Net::HTTP.get(URI.parse(url))
+      parsed_response = JSON.parse(response) rescue {}
+
+      if parsed_response["results"].present?
+        @movies += parsed_response["results"]
+      else
+        break
+      end
+
+      # `total_pages` の値を正しく更新
+      @total_pages = parsed_response["total_pages"] if @total_pages.nil? || parsed_response["total_pages"] > @total_pages
+
+      puts "Fetched #{@movies.count} movies so far."
+      puts "Total available pages: #{@total_pages}"
+    end
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,23 +2,46 @@ class MoviesController < ApplicationController
   require 'net/http'
   require 'json'
 
-  # 安全にAPIリクエストを送るメソッド
-  def fetch_json(url)
-    uri = URI.parse(url)
-    response = Net::HTTP.get(uri)
-    JSON.parse(response) rescue {}  # JSONのパースに失敗したら空のハッシュを返す
-  end
-
   def index
     api_key = ENV['TMDB_API']
+    base_url = "https://api.themoviedb.org/3"
+    total_pages_to_fetch = 5
+    movies_per_page = 20  # 1ページあたりの映画数
+
+
+    @current_page = (params[:page] || 1).to_i
+
     if params[:looking_for].present?
-      movie_title = params[:looking_for]
-      url = "https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&language=ja&query=" + URI.encode_www_form_component(movie_title)
+      search_url = "#{base_url}/search/movie?api_key=#{api_key}&language=ja&query=#{URI.encode_www_form_component(params[:looking_for])}"
     else
-      url = "https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=ja"
+      search_url = "#{base_url}/movie/popular?api_key=#{api_key}&language=ja"
     end
 
-    @movies = fetch_json(url)["results"] || []  # 直接 `results` 配列を取得
+    all_movies = []  # すべての映画を格納する配列
+
+    # 5ページ分のデータを取得
+    (1..total_pages_to_fetch).each do |page|
+      url = "#{search_url}&page=#{page}"
+      response = Net::HTTP.get(URI.parse(url))
+      parsed_response = JSON.parse(response) rescue {}
+
+      if parsed_response["results"].present?
+        all_movies += parsed_response["results"]
+      else
+        break
+      end
+    end
+
+      # トータル映画数を保存
+      @total_movies = all_movies.count
+
+
+      # 1ページに表示する映画を取得
+      start_index = (@current_page - 1) * movies_per_page
+      @movies = all_movies[start_index, movies_per_page] || []
+
+      # トータルページ数を計算（100件を20件ずつに分ける）
+      @total_pages = (@total_movies.to_f / movies_per_page).ceil
   end
 
   def show
