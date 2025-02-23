bdb30e15b1ec06edf66ab700a4b27cc9a5e6b259
yoshiki44 <ys44oct@gmail.com>
2025-02-23 11:31:49 +0900

    お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!  # Deviseなどを使用している場合
+
+  def create
+    favorite = current_user.favorites.create(movie_id: params[:movie_id])
+    redirect_back fallback_location: root_path, notice: "お気に入りに追加しました！"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(movie_id: params[:movie_id])
+    favorite.destroy if favorite
+    redirect_back fallback_location: root_path, notice: "お気に入りを解除しました。"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index ,:show]
   root 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :favorites, only: [:create, :destroy]
 end
