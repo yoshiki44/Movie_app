b07543e2d743658a5877189457c43af25f567c38
yoshiki44 <ys44oct@gmail.com>
2025-02-23 10:48:07 +0900

    favoriteモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_11_122957) do
+ActiveRecord::Schema.define(version: 2025_02_23_014710) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2025_02_11_122957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
 end
