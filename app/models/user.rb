8ca18ab0d309fc98482af769823bd659f5c0c2f6
yoshiki44 <ys44oct@gmail.com>
2025-02-23 10:50:45 +0900

    アソシエーションの追加、favoriteコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_movies, through: :favorites, source: :movie_id
 end
