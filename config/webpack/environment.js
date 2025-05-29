e2694335900c8be5254247fb05f98784079fbe94
yoshiki44 <ys44oct@gmail.com>
2025-05-30 08:35:04 +0900

    fallback設定の全削除

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// Webpack 5 以降で不要な Node.js コアモジュールを無効化
-environment.config.merge({
-  resolve: {
-    fallback: {
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false,
-      dgram: false
-    }
-  }
-})
-
 module.exports = environment
