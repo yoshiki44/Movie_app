03d8fa1427bebb17530ca5f91b9427e3fca70ea1
yoshiki44 <ys44oct@gmail.com>
2025-05-29 22:30:57 +0900

    Fix webpack config and add engines for Heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+// Webpack 5 以降で不要な Node.js コアモジュールを無効化
+environment.config.merge({
+  resolve: {
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+      dgram: false
+    }
+  }
+})
+
 module.exports = environment
